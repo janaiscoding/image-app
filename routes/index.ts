5fc1725dc4bd04a58745801e510bf2bd6c7ffaed
janaiscoding <jana.istrate@gmail.com>
2023-11-02 11:29:30 +0100

    Add mongodb connection, explain project purpose, add views

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req: Request, res: Response, next: NextFunction) {
+  res.render("index", { title: "Express" });
 });
-
+router.post("/upload", (req: Request, res: Response) => {
+  res.json({ message: "This is a post req." });
+});
 export default router;
